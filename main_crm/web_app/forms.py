da9ad84cc11a4272947ed68124207a789d0600ec
andrew26 <75999150+andrew2642@users.noreply.github.com>
2025-08-19 18:35:54 +0300

    Add files via upload

---
+++ main_crm/web_app/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput, TextInput
+from .models import Records
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+
+class loginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+
+def loginForm():
+    return AuthenticationForm
+
+
+class CreateRecordForm(forms.ModelForm):
+    class Meta:
+        model = Records
+        fields = ['first_name', 'last_name', 'email', 'phone', 'skills', 'address', 'category']
+
+
+class UpdateRecordForm(forms.ModelForm):
+    class Meta:
+        model = Records
+        fields = ['first_name', 'last_name', 'email', 'phone', 'skills', 'address', 'category']
